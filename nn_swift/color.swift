abe5a31d97b2e4e386ffc4c8544adc84e126ee93
zoe <z.zoelec2@gmail.com>
2018-02-06 11:27:59 -0800

    Add image support

---
+++ nn_swift/color.swift
@@ -0,0 +1,15 @@
+//
+//  color.swift
+//  nn_swift
+//
+//  Created by Zoe IAMZOE.io on 2/5/18.
+//  Copyright © 2018 Zoe IAMZOE.io. All rights reserved.
+//
+
+import AppKit
+
+extension CGImage {
+    func getPixelColorAt(x:Int, y:Int) -> Double {
+
+    }
+}
